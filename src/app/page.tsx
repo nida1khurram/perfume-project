b42adab8bf14b4e2c3a8a589facc8103cea2bc84
nida1khurram <147607279+nida1khurram@users.noreply.github.com>
2024-10-02 22:14:57 +0500

    "style folder deleted"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Hero from "@/components/hero";
-import Image from "next/image";
+
 
 export default function Home() {
   return (
--- src/components/imagesSlider.tsx
+++ src/components/imagesSlider.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useRef, useState } from 'react';
+import React  from 'react';
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
 
@@ -12,14 +12,16 @@ import 'swiper/css/navigation';
 import { Navigation } from 'swiper/modules';
 
 // import swiper slide
-import style from '@/app/style/swiper.module.css'
+// import style from '@/app/style/swiper.module.css'
 
 import Image from 'next/image';
 function Imageslider() {
   return (
     <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-      {/*  slide */}
-      <SwiperSlide className={style.SwiperSlide}>
+      {/*  slide use global css*/}
+
+      {/* <SwiperSlide className={style.SwiperSlide}> */}
+      <SwiperSlide className={'swiperSlide'}>
     <Image 
      className=' object-center  w-[100%] h-[500px]'
      alt="slider"
@@ -28,7 +30,9 @@ function Imageslider() {
       src={require("../../picture/slider/pic-1.jpg")} />
     </SwiperSlide>   
     {/*  slide */}
-    <SwiperSlide className={style.SwiperSlide}>
+
+    {/* <SwiperSlide className={style.SwiperSlide}> */}
+    <SwiperSlide className={'swiperSlide'}>
     <Image 
     //  className='object-cover object-center rounded mx-auto w-[15rem]'
      className=' object-center  w-[100%] h-[500px]'
@@ -38,7 +42,8 @@ function Imageslider() {
       src={require("../../picture/slider/pic-2.jpg")} />
     </SwiperSlide>
     {/*  slide */}
-    <SwiperSlide className={style.SwiperSlide}>
+    {/* <SwiperSlide className={style.SwiperSlide}> */}
+    <SwiperSlide className={'swiperSlide'}>
     <Image 
     //  className='object-cover object-center rounded mx-auto w-[15rem]'
      className=' object-center  w-[100%] h-[500px]'
@@ -48,7 +53,8 @@ function Imageslider() {
       src={require("../../picture/slider/pic-3.jpg")} />
     </SwiperSlide>
     {/*  slide */}
-    <SwiperSlide className={style.SwiperSlide}>
+    {/* <SwiperSlide className={style.SwiperSlide}> */}
+    <SwiperSlide className={'swiperSlide'}>
     <Image 
     //  className='object-cover object-center rounded mx-auto w-[15rem]'
      className=' object-center  w-[100%] h-[500px]'
@@ -58,7 +64,8 @@ function Imageslider() {
       src={require("../../picture/slider/pic-4.jpg")} />
     </SwiperSlide>
     {/*  slide */}
-    <SwiperSlide className={style.SwiperSlide}>
+    {/* <SwiperSlide className={style.SwiperSlide}> */}
+    <SwiperSlide className={'swiperSlide'}>
     <Image 
     //  className='object-cover object-center rounded mx-auto w-[15rem]'
      className=' object-center  w-[100%] h-[500px]'
@@ -68,7 +75,8 @@ function Imageslider() {
       src={require("../../picture/slider/pic-5.jpg")} />
     </SwiperSlide>
     {/*  slide */}
-    <SwiperSlide className={style.SwiperSlide}>
+    {/* <SwiperSlide className={style.SwiperSlide}> */}
+    <SwiperSlide className={'swiperSlide'}>
     <Image 
     //  className='object-cover object-center rounded mx-auto w-[15rem]'
      className=' object-center  w-[100%] h-[500px]'
